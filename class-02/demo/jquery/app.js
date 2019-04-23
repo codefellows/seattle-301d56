49cde9fcf97cce1dc991bfc9f8f98eee0885fa9e
mnfmnfm <mnfmnfm@gmail.com>
2019-04-23 16:05:10 -0700

    finish toggling in vanilla

--- class-02/demo/jquery/app.js
+++ class-02/demo/jquery/app.js
@@ -1,20 +1,7 @@
 console.log('linked');
 
-let getTwo = () => 2;
-
-let add = (a, b) => a + b;
-
-let logThing = thing => console.log(thing);
-
-let logResult = thing => console.log(thing());
-
-// an array
-// a function
-//     the thing to do on each element
-let michelleForEach = (arr, behavior) => {
-  for(let i = 0; i < arr.length; i++) {
-    behavior(arr[i], i, arr);
-  }
-};
-
-
+document.getElementsByClassName('toggler')[0].addEventListener('click', function(e) {
+  e.preventDefault();
+  let hidable = document.getElementsByClassName('hidable')[0];
+  hidable.classList.toggle('is-hidden');
+});
