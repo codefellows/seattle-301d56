1d8ddebe3e0cf2db56175945b1c38821e7f3a8a5
mnfmnfm <mnfmnfm@gmail.com>
2019-04-24 15:45:49 -0700

    add handlebars

--- class-03/demo/app.js
+++ class-03/demo/app.js
@@ -5,10 +5,14 @@ $('.toggler').on('click', function(e) {
   $('.hidable').toggle();
 });
 
+// liRenderer is a function that takes in an object and gives back a string
+const liRenderer = Handlebars.compile($('#li-template').text());
+
 // grab data and render
 $.get('./data.json', data => {
   console.log(data);
   let ul = $('ul');
   ul.empty();
-  data.forEach(day => ul.append(`<li>${day.day}: ${day.description}</li>`));
+  //data.forEach(day => ul.append(`<li>${day.day}: ${day.description}</li>`));
+  data.forEach(day => ul.append(liRenderer(day)));
 });
