2b351ac3c5dd671a06fca0df78d6daae55f93958
mnfmnfm <mnfmnfm@gmail.com>
2019-04-26 15:54:46 -0700

    add deployment files

---
+++ class-05/workshop/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+app.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained'
+  }
+  response.status(200).json(airplanes);
+});
+
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
