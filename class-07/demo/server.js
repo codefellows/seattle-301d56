6d913b42341189e2d015b1c7af5def8e5f18fda3
mnfmnfm <mnfmnfm@gmail.com>
2019-04-30 16:41:33 -0700

    demo code working with gmaps

--- class-07/demo/server.js
+++ class-07/demo/server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // Application Setup
 const PORT = process.env.PORT || 3000;
@@ -16,8 +17,21 @@ app.use(cors());
 
 app.get('/location', (request, response) => {
   try {
-    const locationData = searchToLatLong(request.query.data);
-    response.send(locationData);
+    // queryData is what the user typed into the box in the FE and hit "explore"
+    const queryData = request.query.data;
+    // const geoData = require('./data/geo.json');
+    // json files are so old school
+    // make a request to the Google Geocoding API for geocoding data
+    let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+    superagent.get(geocodeURL)
+      // when the data comes back from the gmaps api
+      .end( (err, googleMapsApiResponse) => {
+        console.log(googleMapsApiResponse.body);
+        // turn it into a location instance
+        const location = new Location(queryData, googleMapsApiResponse.body);
+        // send that as our response to our frontend
+        response.send(location);
+      });
   }
   catch(error) {
     console.error(error);
@@ -38,21 +52,11 @@ app.get('/weather', (request, response) => {
 
 // Helper Functions
 
-function searchToLatLong(query) {
-  // query = 'there'
-
-  const geoData = require('./data/geo.json');
-  console.log('geoData is', geoData);
-  const location = new Location(query, geoData);
-  // location has search_query, formatted_query, latitude, longitude
-  return location;
-}
-
 function Location(query, res) {
   this.search_query = query;
   this.formatted_query = res.results[0].formatted_address;
-  this.Latitude = res.results[0].geometry.location.lat;
-  this.Longitude = res.results[0].geometry.location.lng;
+  this.latitude = res.results[0].geometry.location.lat;
+  this.longitude = res.results[0].geometry.location.lng;
 }
 
 function getWeather() {
